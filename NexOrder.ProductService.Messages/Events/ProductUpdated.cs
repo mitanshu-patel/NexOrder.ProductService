dc3b3ea8c8f516d613b459d3fe4d42133fb5c7a5
Mitanshu Patel <mtg865@gmail.com>
2026-04-02 13:03:30 +0530

    test commit

--- NexOrder.ProductService.Messages/Events/ProductUpdated.cs
+++ NexOrder.ProductService.Messages/Events/ProductUpdated.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NexOrder.ProductService.Messages.Events
+﻿namespace NexOrder.ProductService.Messages.Events
 {
     public record ProductUpdated(int Id, string Name, string Description, decimal Price);
 }
